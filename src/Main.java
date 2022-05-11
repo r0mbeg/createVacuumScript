e3beefa41f9a983a9f7280a407c8dd06b1f08377
Roman <roma564888@yandex.ru>
2022-05-11 16:03:46 +0300

    Removed last change time check

--- src/Main.java
+++ src/Main.java
@@ -40,49 +40,14 @@ public class Main {
         file.createNewFile();
         FileWriter writer = new FileWriter(file);
         writer.write("Taskkill /IM SQLiteStudio.exe /F" + "\n");
-
         writer.write("set /a db_counter = 0 \n");
         writer.write("set /a vacuumed_counter = 0 \n");
-
         for (int i = 0; i < dbPaths.size(); i++) {
             writer.write("echo --------------------------" + dbNames.get(i).toUpperCase() + "-------------------------- >> " + logPath + "\n");
-
             writer.write("cd /d " + dbDirectories.get(i) + "\n");
             //начинаем процесс бекапа и вакуума только если база использовалась давно
-
             writer.write("set /a db_counter += 1 \n");
             writer.write("set /a vacuumed_counter += 1 \n");
-            writer.write("set N=60" + "\n");
-            writer.write("set file=" + dbPaths.get(i) + "\n");
-            writer.write("set /a const=%N%*60" + "\n");
-            writer.write("for /f \"usebackq delims=\" %%I in ('\"%file%\"') do set A=%%~tI" + "\n");
-            writer.write("set mon=%a:~3,2%" + "\n");
-            writer.write("set D=%a:~0,2%" + "\n");
-            writer.write("set h=%a:~11,2%" + "\n");
-            writer.write("set m=%a:~14,2%" + "\n");
-            writer.write("set /a Hs=H*3600" + "\n");
-            writer.write("set /a Ms=M*60" + "\n");
-            writer.write("set /a Ts=Hs+Ms" + "\n");
-            writer.write("set now=%DATE: =0% %TIME: =0%" + "\n");
-            writer.write("set mon1=%now:~3,2%" + "\n");
-            writer.write("set D1=%now:~0,2%" + "\n");
-            writer.write("set H1=%now:~11,2%" + "\n");
-            writer.write("set M1=%now:~14,2%" + "\n");
-            writer.write("set /a Hs1=H1*3600" + "\n");
-            writer.write("set /a Ms1=M1*60" + "\n");
-            writer.write("set /a Ts1=Hs1+Ms1" + "\n");
-            writer.write("if %mon1% GTR %mon% (set /a Mons=D*86400) else set Mons=0" + "\n");
-            writer.write("set /a TimeI=Ts+mons" + "\n");
-            writer.write("set /a y=d+d1" + "\n");
-            writer.write("if %mon1% GTR %mon% (set /a Mons1=y*86400) else set Mons1=0" + "\n");
-            writer.write("If %D1% GTR %D% (set Ds1=86400) else set Ds1=0" + "\n");
-            writer.write("set /a TimeT=Ts1+Ds1+Mons1-const-TimeI" + "\n");
-            writer.write("set /a mm=TimeT/60" + "\n");
-            writer.write("set /a n2=mm+n" + "\n");
-            writer.write("set /a n1=timet+n*60" + "\n");
-            writer.write("if %n2% GEQ %N% (" + "\n");
-
-            writer.write("echo The difference is more than %n% minutes, starting vacuumization! >> " + logPath + "\n");
             writer.write("echo 1. A new vacuumization started %date% at %time% >> " + logPath + "\n");
             if (archivation == "winrar") {
                 writer.write("if exist " + dbNames.get(i)+ "_backup_*.rar forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_backup_*.rar /d -32 /c \"cmd /c del @file\"\n");
@@ -119,9 +84,6 @@ public class Main {
             writer.write("curl https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + chat_id + "^^^&text=" + "\"Database " + dbNames.get(i) + ", number " + i + ", at LPU " + lpuName + " wasn't completely vacuumed!\" \n");
             writer.write("set /a vacuumed_counter -= 1 )\n");
             writer.write("echo 7. The old base was deleted %time% >> " + logPath + "\n");
-            writer.write(") else ( \n" +
-                    "echo The difference is less than %n% minutes, vacuumization does not begin >> " + logPath + "\n");
-            writer.write("set /a vacuumed_counter -= 1 )\n");
             writer.write("echo ------------------------------------------------------------- >> " + logPath + "\n" + "\n");
         }
         writer.write("cd /d " + curlPath + "\n");
