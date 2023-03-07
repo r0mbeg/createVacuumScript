cba0c687dddb8aed58fb6e4f32fe6c5389b193bb
Roman <roma564888@yandex.ru>
2023-03-07 12:07:53 +0300

    Changed archivation

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class Main {
+public class Main extends bot_config{
+
     public static void main(String[] args) throws IOException {
         String desktopDirectory = System.getProperty("user.home") + "\\Desktop";
-        String botToken = "5195909558:AAHeO85DtxK_j09Y3i4cM1KkE4obSr7ng9w";
-        String chat_id = "-1001760421137";
-        String lpuName = "p94";
+
+        String lpuName = "p49";
         String archivation = "winrar";//winrar 7z no
+
         ArrayList<String> inputList = new ArrayList<>();
         try {
             File inputFile = new File(desktopDirectory + "\\input.txt");
@@ -56,27 +57,15 @@ public class Main {
             writer.write("if exist sqlite3.exe ( \n");
             writer.write("set /a vacuumed_counter += 1 \n");
             writer.write("echo 1. A new vacuumization started %date% at %time% >> " + logPath + "\n");
-            if (archivation == "winrar") {
-                writer.write("if exist " + dbNames.get(i)+ "_backup_*.rar forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_backup_*.rar /d -32 /c \"cmd /c del @file\"\n");
-            } else if (archivation == "7z") {
-                writer.write("if exist " + dbNames.get(i)+ "_backup_*.7z forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_backup_*.7z /d -32 /c \"cmd /c del @file\"\n");
-            } else if (archivation == "no") {
-                writer.write("if exist " + dbNames.get(i) + "_backup_*.db forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_backup_*.db /d -32 /c \"cmd /c del @file\"\n");
-            }
+            writer.write("if exist " + dbNames.get(i)+ "_backup_*.rar forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_backup_*.rar /d -32 /c \"cmd /c del @file\"\n");
+            writer.write("if exist " + dbNames.get(i)+ "_backup_*.7z forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_backup_*.7z /d -32 /c \"cmd /c del @file\"\n");
+            writer.write("if exist " + dbNames.get(i) + "_backup_*.db forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_backup_*.db /d -32 /c \"cmd /c del @file\"\n");
             //очистка архивов
             writer.write("if exist " + dbNames.get(i)+ "_archive_*.db forfiles /p " + dbDirectories.get(i) + " /m " + dbNames.get(i) + "_archive_*.db /d -5 /c \"cmd /c del @file\"\n");
             writer.write("echo 2. The old backups and archives were deleted at %time% >>  " + logPath + "\n");
             writer.write("ren " + dbNames.get(i) + ".db " + dbNames.get(i) + "_copy.db" + "\n");
             writer.write("sqlite3 \"" + dbNames.get(i) + "_copy.db\" \".backup '" + dbNames.get(i) + "_backup_%date%.db'\"" + "\n");
             writer.write("echo 3. A new backup was created at %time% >> " + logPath + "\n");
-            //архивация бэкапа, если есть архиватор
-            if (archivation == "winrar") {
-                writer.write("\"" + archiverPath + "\\winrar.exe" + "\" a " + dbNames.get(i) + "_backup_%date%.rar " + dbNames.get(i) + "_backup_%date%.db" + "\n");
-                writer.write("if exist " + dbNames.get(i) + "_backup_%date%.rar del " + dbNames.get(i) + "_backup_%date%.db" + "\n");
-            } else if (archivation == "7z") {
-                writer.write("\"" + archiverPath + "\\7z.exe" + "\" a " + dbNames.get(i) + "_backup_%date%.7z " + dbNames.get(i) + "_backup_%date%.db" + "\n");
-                writer.write("if exist " + dbNames.get(i) + "_backup_%date%.7z del " + dbNames.get(i) + "_backup_%date%.db" + "\n");
-            }
             writer.write("if exist " + dbNames.get(i) + "_vacuumed.db del " + dbNames.get(i) + "_vacuumed.db" + "\n");
             writer.write("sqlite3 \"" + dbNames.get(i) + "_copy.db\" \"VACUUM INTO '" + dbNames.get(i) + "_vacuumed.db'\"" + "\n");
             writer.write("echo 4. The base was vacuumized at %time% >> " + logPath + "\n");
@@ -85,10 +74,8 @@ public class Main {
             writer.write("if exist " + dbNames.get(i) + "_vacuumed.db " + "ren " + dbNames.get(i) + "_vacuumed.db " + dbNames.get(i) +".db\n");
             writer.write("echo 6. The vacuumized base was renamed to the main at %time% >> " + logPath + "\n");
             writer.write("if exist " + dbNames.get(i) + ".db del " + dbNames.get(i) + "_copy.db\n");
-
             writer.write("if exist " + dbNames.get(i) + "_vacuumed.db set /a res = 0\n");
             writer.write("if exist " + dbNames.get(i) + "_copy.db set /a res = 0\n");
-
             writer.write("if %res% == 0 ( \n");
             writer.write("echo ALERT The base " + dbNames.get(i) + ", number " + i + " at LPU " + lpuName + " wasn't completely vacuumed! >> " + logPath + "\n");
             writer.write("cd /d " + curlPath + " \n");
@@ -98,11 +85,32 @@ public class Main {
             writer.write(")\n");
             writer.write("echo ------------------------------------------------------------- >> " + logPath + "\n" + "\n");
         }
+
+
+        writer.write("iisreset /start" + "\n");
+
+
+        writer.write("echo -----------------BACKUPS-ARCHIVATION-STARTS-------------------- >> " + logPath + "\n");
+        for (int i = 0; i < dbPaths.size(); i ++) {
+            writer.write("echo  Archivation of the base " + dbNames.get(i) + ", number " + i +" started %date% at %time% >> " + logPath + "\n");
+            writer.write("cd /d " + dbDirectories.get(i) + "\n");
+            if (archivation == "winrar") {
+                writer.write("\"" + archiverPath + "\\winrar.exe" + "\" a " + dbNames.get(i) + "_backup_%date%.rar " + dbNames.get(i) + "_backup_%date%.db" + "\n");
+                writer.write("if exist " + dbNames.get(i) + "_backup_%date%.rar del " + dbNames.get(i) + "_backup_%date%.db" + "\n");
+            } else if (archivation == "7z") {//лучше!!!
+                writer.write("\"" + archiverPath + "\\7z.exe" + "\" a " + dbNames.get(i) + "_backup_%date%.7z " + dbNames.get(i) + "_backup_%date%.db" + "\n");
+                writer.write("if exist " + dbNames.get(i) + "_backup_%date%.7z del " + dbNames.get(i) + "_backup_%date%.db" + "\n");
+            }
+            writer.write("echo  Archivation of the base " + dbNames.get(i) + ", number " + i +" ended %date% at %time% >> " + logPath + "\n");
+        }
+        writer.write("echo -----------------BACKUPS-ARCHIVATION-ENDS-------------------- >> " + logPath + "\n" + "\n");
+
+
         writer.write("cd /d " + curlPath + "\n");
         writer.write("set message=\"%vacuumed_counter% out of %db_counter% bases at LPU " + lpuName + " were vacuumized!\"" + "\n");
         writer.write("echo %vacuumed_counter% out of %db_counter% bases were vacuumized! >> " + logPath + "\n");
         writer.write("curl https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + chat_id + "^^^&text=%message%"+ "\n");
-        writer.write("iisreset /start" + "\n");
+
 
         writer.flush();
         writer.close();
---
+++ src/bot_config.java
@@ -0,0 +1,4 @@
+public class bot_config {
+    public static String botToken = "5195909558:AAHeO85DtxK_j09Y3i4cM1KkE4obSr7ng9w";
+    public static String chat_id = "-1001760421137";
+}
